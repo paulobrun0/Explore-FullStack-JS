01aa2bc34c4cb1d69e1f37d0ef09ff4389673461
paulobrun0 <paulo.bruno04@hotmail.com>
2024-08-28 22:11:14 -0300

    Operador de coalescência nula

--- Intermediário/OperadorCoalescenciaNula.js
+++ Intermediário/OperadorCoalescenciaNula.js
@@ -2,3 +2,10 @@ let content = "test";
 // Funciona apenas com null ou undefined
 //Operador de coalescência nula
 console.log(content ?? "Conteúdo Padrao");
+
+const user = {
+  name: "Paulo",
+  picture: undefined,
+};
+
+console.log(user.picture ?? "default.png");
