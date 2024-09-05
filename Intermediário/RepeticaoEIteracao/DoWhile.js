ae93e20c411c2ad38bb8bfa7b752c781c11ea52e
paulobrun0 <paulo.bruno04@hotmail.com>
2024-09-04 22:39:30 -0300

    Do while

---
+++ Intermediário/RepeticaoEIteracao/DoWhile.js
@@ -0,0 +1,10 @@
+// Do While: repete até que a condição especificada seja VERDADEIRA. No entanto, a instrução será executada pelo menos uma vez antes da condição ser verificada
+
+let value = 11;
+
+do {
+  value++;
+  console.log(value);
+} while (value < 10);
+
+console.log("Segue fluxo...");
