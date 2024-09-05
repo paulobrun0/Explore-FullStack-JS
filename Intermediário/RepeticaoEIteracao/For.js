a40d90fde73680e1ffd5398b0f149cd86c87598b
paulobrun0 <paulo.bruno04@hotmail.com>
2024-09-04 22:49:24 -0300

    Loop For

---
+++ Intermediário/RepeticaoEIteracao/For.js
@@ -0,0 +1,12 @@
+// For repete até que a condição especificada seja FALSA
+
+for (let step = 0; step < 10; step++) {
+  console.log("Step: ", step);
+}
+
+// Exemplo de tabuada.
+console.log("### TABUADA ###");
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`7 x ${i}: ${7 * i}`);
+}
