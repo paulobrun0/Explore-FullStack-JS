c8c0f48fb8c7ded5a6bd9bbd2f76872b4efa2a0c
paulobrun0 <paulo.bruno04@hotmail.com>
2024-09-11 23:43:58 -0300

    projeto refound iniciado

---
+++ Intermediário/ProjetoRefound/scripts.js
@@ -0,0 +1,56 @@
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+const expenseList = document.querySelector("ul");
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "");
+
+  value = Number(value) / 100;
+
+  amount.value = formatCurrencyBRL(value);
+};
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return value;
+}
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date().toISOString(),
+  };
+
+  expenseAdd(newExpense);
+
+  expense.value = "";
+};
+
+function expenseAdd(newExpense) {
+  try {
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    expenseItem.appendChild(expenseIcon);
+    expenseList.appendChild(expenseItem);
+  } catch (error) {
+    alert("Ocorreu um erro ao adicionar a despesa");
+    console.error(error);
+  }
+}
