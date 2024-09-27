c8a710a64a9348c3a78695adc3c6ea34a0b122fa
paulobrun0 <paulo.bruno04@hotmail.com>
2024-09-27 08:31:01 -0300

    Trabalhando com o método some

---
+++ Avançado/MetodosArray/Some.js
@@ -0,0 +1,7 @@
+// O método some() testa se ao menos um dos elementos no array passa na condição e retorna um valor true ou false.
+
+// Exemplo de array de idades
+const ages = [18, 21, 25, 15];
+
+const result = ages.some((age) => age < 18);
+console.log(result);
