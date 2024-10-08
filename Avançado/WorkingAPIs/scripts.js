da5f9579a26d61328bd1f3d27a963d318b2cfb53
paulobrun0 <paulo.bruno04@hotmail.com>
2024-10-07 22:02:05 -0300

    Utilizando o fetch com async/ await e trabalhando com parametros

--- Avançado/WorkingAPIs/scripts.js
+++ Avançado/WorkingAPIs/scripts.js
@@ -1,3 +1,21 @@
-fetch("http://localhost:3333/products")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+// Utilizando o fetch com o then
+// fetch("http://localhost:3333/products")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// Utilizando o fetch com async/await
+
+async function fetchProducts() {
+  const response = await fetch("http://localhost:3333/products");
+  const data = await response.json();
+  console.log(data);
+}
+
+async function fetchProduct(id) {
+  const response = await fetch(`http://localhost:3333/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+// fetchProducts();
+fetchProduct(2);
