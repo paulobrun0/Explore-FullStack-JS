262f15812f8d583fa52c1839336d4613ce2ea917
paulobrun0 <paulo.bruno04@hotmail.com>
2024-10-17 23:34:50 -0300

    tipagem de arrays

---
+++ TypeScript/src/TipagemArrays.ts
@@ -0,0 +1,6 @@
+// Tipagem explicita ou Anotação de tipo(Type Annotation)
+let names: string[] = ["Paulo", "Vitor", "Rodrigo"];
+let numbers: number[] = [1, 2, 3];
+
+// Inferência de tipos
+let products = ["Product x", "Product y", "Product z"];
--- TypeScript/src/index.ts
+++
@@ -1,6 +0,0 @@
-let mensagem;
-
-// Com o tipo any a variável aceitar qualquer valor.
-mensagem = "Esse é um texto";
-mensagem = 10;
-mensagem = true;
