be928d79dfb205d7b8574429857974a0d7749648
paulobrun0 <paulo.bruno04@hotmail.com>
2024-10-17 23:53:04 -0300

    Null e Undefined

---
+++ TypeScript/src/NullUndefined.ts
@@ -0,0 +1,18 @@
+// undefined = nao teve valor atribuido
+let value: number;
+value = 12;
+console.log(value);
+
+let user1 = {
+  name: "John Doe",
+};
+
+// console.log(user1.email);
+
+// Null = definir intencionalmente a ausencia de um valor
+let email = null;
+console.log(email);
+
+if (!email) {
+  console.log("Informe o e-mail!");
+}
