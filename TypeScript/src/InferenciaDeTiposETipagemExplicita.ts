f2d9db9bf5e5e0ef542b1c23895c3d63c7527710
paulobrun0 <paulo.bruno04@hotmail.com>
2024-10-17 23:26:05 -0300

    Inferencia de tipos e tipagem explicita

---
+++ TypeScript/src/InferenciaDeTiposETipagemExplicita.ts
@@ -0,0 +1,8 @@
+// Tipagem explicita
+let myName: string;
+
+myName = "Paulo Bruno";
+
+// Inferência de Tipos
+let message = "Oi, tudo bem?";
+message = "Sim. Tudo ótimo!";
---
+++ TypeScript/src/tiposPrimitivos.ts
@@ -0,0 +1,18 @@
+// String para texto
+let username: string;
+
+username = "rodrigo";
+// username = 17
+// username = true
+
+// Number para número
+let total: number;
+
+total = 3;
+total = 7.5;
+
+// Boolean para verdadeiro ou falso
+let isLoading: boolean;
+
+isLoading = true;
+isLoading = false;
