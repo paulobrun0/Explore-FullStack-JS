db9eb66453c759b8938d420284c5137503edff91
paulobrun0 <paulo.bruno04@hotmail.com>
2024-10-28 22:57:34 -0300

    salvando em memoria e criando o arquivo

--- Node/FundamentosAPI/src/routes.js
+++ Node/FundamentosAPI/src/routes.js
@@ -3,22 +3,28 @@ export const routes = [
   {
     method: "GET",
     path: "/products",
-    controller: (request, response) => {
-      response.end(JSON.stringify(request.query));
+    controller: ({ request, response, database }) => {
+      const products = database.select("products");
+
+      return response.end(JSON.stringify(products));
     },
   },
   {
     method: "POST",
     path: "/products",
-    controller: (request, response) => {
+    controller: ({ request, response, database }) => {
+      const { name, price } = request.body;
+
+      database.insert("products", { name, price });
+
       return response.writeHead(201).end(JSON.stringify(request.body));
     },
   },
   {
     method: "DELETE",
     path: "/products/:id",
-    controller: (request, response) => {
-      return response.end("Produto removido com ID : " + request.params.id);
+    controller: ({ request, response }) => {
+      return response.end(request.params.id);
     },
   },
 ].map((router) => ({
