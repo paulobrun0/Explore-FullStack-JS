0102de45ebd3f62a1a83f33a728dbf1eb0d8c457
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-16 13:39:41 -0300

    node watch

--- Node/Iniciando com Nodejs/src/server.js
+++ Node/Iniciando com Nodejs/src/server.js
@@ -2,7 +2,7 @@ import httt from "node:http";
 
 const server = httt.createServer((request, response) => {
   // Devolve a resposta e escreve "Hello world!" no console
-  return response.end("Hello World!");
+  return response.end("Resposta do servidor, Sucesso");
 });
 
 server.listen(3333);
