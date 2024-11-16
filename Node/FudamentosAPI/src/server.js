581387ec80c52de7026f425028ab21ab550b8bd0
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-16 16:00:42 -0300

    Recuperando dados no body

--- Node/FudamentosAPI/src/server.js
+++ Node/FudamentosAPI/src/server.js
@@ -1,6 +1,6 @@
 import http from "node:http";
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
   if (method === "GET" && url === "/products") {
@@ -8,6 +8,16 @@ const server = http.createServer((request, response) => {
   }
 
   if (method === "POST" && url === "/products") {
+    const buffer = [];
+
+    // Recebe dados do body
+    for await (const chunk of request) {
+      buffer.push(chunk);
+    }
+
+    // Converte buffer para string de forma legível
+    console.log(Buffer.concat(buffer).toString());
+
     return response.writeHead(201).end("Produto Cadastrado!");
   }
 
