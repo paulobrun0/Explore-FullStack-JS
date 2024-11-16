bc9d8cd4e2bf717907504ff8db9ffd9273c52c5a
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-16 14:36:09 -0300

    Utilizando status code

--- Node/FudamentosAPI/src/server.js
+++ Node/FudamentosAPI/src/server.js
@@ -2,7 +2,7 @@ import http from "node:http";
 
 const server = http.createServer((request, response) => {
   const { method } = request;
-  return response.end("Metodo " + method);
+  return response.writeHead(200).end("Metodo " + method);
 });
 
 server.listen(3333);
