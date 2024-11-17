18fca6304a5b63194d9085fdf7f9ca69cd0970f3
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-17 19:20:01 -0300

    Filtrando pelo status

--- Node/PrimeiroProjeto/src/controllers/tickets/index.js
+++ Node/PrimeiroProjeto/src/controllers/tickets/index.js
@@ -1,7 +1,8 @@
 export function index({ request, response, database }) {
   const { status } = request.query;
-  console.log(status);
 
-  const tickets = database.select("tickets");
+  const filters = status ? { status: status } : null;
+
+  const tickets = database.select("tickets", filters);
   return response.writeHead(200).end(JSON.stringify(tickets));
 }
