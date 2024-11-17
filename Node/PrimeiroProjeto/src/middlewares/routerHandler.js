d20b8d060d5210521df2bf2bbd2aad59a3445445
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-17 17:11:29 -0300

    Criando o banco de dados

--- Node/PrimeiroProjeto/src/middlewares/routerHandler.js
+++ Node/PrimeiroProjeto/src/middlewares/routerHandler.js
@@ -1,11 +1,14 @@
 import { routes } from "../routes/index.js";
+import { Database } from "../database/database.js";
+
+const database = new Database();
 export function routerHandler(request, response) {
   const route = routes.find((route) => {
     return route.method === request.method && route.path === request.url;
   });
 
   if (route) {
-    return route.controller({ request, response });
+    return route.controller({ request, response, database });
   }
 
   return response.writeHead(404).end();
