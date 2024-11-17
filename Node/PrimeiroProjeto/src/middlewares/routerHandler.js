854d53869ee43bafa2db8bd0e0c2d06d9903c68d
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-17 16:44:04 -0300

    Middleware de rotas

---
+++ Node/PrimeiroProjeto/src/middlewares/routerHandler.js
@@ -0,0 +1,12 @@
+import { routes } from "../routes/index.js";
+export function routerHandler(request, response) {
+  const route = routes.find((route) => {
+    return route.method === request.method && route.path === request.url;
+  });
+
+  if (route) {
+    return route.controller(request, response);
+  }
+
+  return response.writeHead(404).end();
+}
--- Node/PrimeiroProjeto/src/server.js
+++ Node/PrimeiroProjeto/src/server.js
@@ -1,10 +1,11 @@
 import http from "node:http";
 
 import { jsonHandler } from "./middlewares/jsonHandler.js";
+import { routerHandler } from "./middlewares/routerHandler.js";
 
 async function listener(request, response) {
   await jsonHandler(request, response);
-  console.log(request.body);
+  routerHandler(request, response);
 }
 
 http.createServer(listener).listen(3333);
