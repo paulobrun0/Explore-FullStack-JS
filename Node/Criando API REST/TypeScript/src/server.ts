3901f055a8cda07f45a7052464498bf441fc20ff
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-22 11:59:31 -0300

    Query Params

--- Node/Criando API REST/TypeScript/src/server.ts
+++ Node/Criando API REST/TypeScript/src/server.ts
@@ -4,12 +4,15 @@ const PORT = 3333;
 
 const app = express();
 
-app.get("/products/:id/:user", (request, response) => {
-  // Pega o id do produto
-  const { id, user } = request.params;
+app.get("/products", (request, response) => {
+  // products?page=1&limit=10
+
+  // Pegando os parametros da requisição
+
+  const { page, limit } = request.query;
 
   // Devolve a resposta
-  response.send(`Produto ${id} do usuário ${user} `);
+  response.send(`Página ${page} de ${limit}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
