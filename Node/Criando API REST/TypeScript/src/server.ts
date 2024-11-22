8d07bd00ad68743dd4b92551daad32e313e4265a
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-22 11:55:28 -0300

    Route Params

--- Node/Criando API REST/TypeScript/src/server.ts
+++ Node/Criando API REST/TypeScript/src/server.ts
@@ -4,9 +4,12 @@ const PORT = 3333;
 
 const app = express();
 
-app.get("/", (request, response) => {
+app.get("/products/:id/:user", (request, response) => {
+  // Pega o id do produto
+  const { id, user } = request.params;
+
   // Devolve a resposta
-  response.send("Hello World Express");
+  response.send(`Produto ${id} do usuário ${user} `);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
