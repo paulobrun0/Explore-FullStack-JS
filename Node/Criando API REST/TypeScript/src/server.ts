6cf3d4b5477f3c85aeab2ea3697cff3652a79011
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-22 11:49:07 -0300

    Enviando requisição com o método GET

--- Node/Criando API REST/TypeScript/src/server.ts
+++ Node/Criando API REST/TypeScript/src/server.ts
@@ -4,4 +4,9 @@ const PORT = 3333;
 
 const app = express();
 
+app.get("/", (request, response) => {
+  // Devolve a resposta
+  response.send("Hello World Express");
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
