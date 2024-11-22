2b0368ee03c12af747ae854122c06cd985f2bd5d
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-22 12:09:39 -0300

    Resposta com JSON e Status Code

--- Node/Criando API REST/TypeScript/src/server.ts
+++ Node/Criando API REST/TypeScript/src/server.ts
@@ -20,7 +20,9 @@ app.get("/products", (request, response) => {
 app.post("/products", (request, response) => {
   const { name, price } = request.body;
 
-  response.send(`Produto ${name} com preço de ${price}`);
+  // response.send(`Produto ${name} com preço de ${price}`);
+
+  response.status(201).json({ name, price });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
