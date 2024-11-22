f45f43cab1c58acf675c9370d68e2e92da3a44fc
paulobrun0 <paulo.bruno04@hotmail.com>
2024-11-22 12:29:26 -0300

    Middleware Global

--- Node/Criando API REST/TypeScript/src/server.ts
+++ Node/Criando API REST/TypeScript/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
-
+import { myMiddleware } from "./middlewares/my-middleware";
 const PORT = 3333;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(myMiddleware);
 
 app.get("/products", (request, response) => {
   // products?page=1&limit=10
